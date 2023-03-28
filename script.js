0d6519db74deed6e75d9d57c047be551a22de9ee
d3stroyer113 <119396048+d3stroyer113@users.noreply.github.com>
2023-03-28 09:30:07 +0300

    Kods bez stiliem
    Šo darbu veidojam otrajā stundā

---
+++ script.js
@@ -0,0 +1,13 @@
+const coolButton = document.querySelector('.js-cool');
+
+const dontButton = document.querySelector('.js-dont')
+
+coolButton.addEventListener('mouseover',
+() => {
+    window.open('https://www.youtube.com/watch?v=jxEigw8RQ5k')
+})
+
+dontButton.addEventListener('mouseover',
+() => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+})
